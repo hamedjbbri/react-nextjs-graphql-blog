7b2eb695607e62a59692cb21b82df20aea650208
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-11-11 20:18:27 -0800

    API access

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,10 @@
 import Head from 'next/head'
 import { PostCard, Categories, PostWidget } from '../components';
+import { getPosts } from '../services';
 
+ 
 
-const posts = [
-  {
-    title: 'Hello Next.js ', excerpt: 'This is my first post'
-  },
-  {
-    title: 'React Tailwind ', excerpt: 'Learn react with tailwind'
-  },
-];
-
-
-export default function Home() {
+export default function Home( { posts } ) {
   return (
     <div className="container mx-auto px-10 mb-8 ">
       <Head>
@@ -33,4 +25,12 @@ export default function Home() {
       </div>
     </div>
   )
+}
+
+
+export async function getStaticProps() {
+  const posts = (await getPosts()) || [];
+  return {
+    props: { posts}
+  }
 }
