662452dc51d0100be2744f76547e8f75085b2b8d
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-11-14 10:00:26 -0800

    categories and nav

--- services/index.js
+++ services/index.js
@@ -37,4 +37,64 @@ export const getPosts = async () => {
   const result = await request(graphqlAPI, query);
 
   return result.postsConnection.edges;
+};
+
+
+export const getRecentPosts = async () => {
+    const query = gql`
+    query GetPostDetails() {
+        posts(
+            orderBy: createdAt_ASC
+            last: 3
+            ){
+                title
+                featuredImage{
+                    url
+                }
+                slug
+                createdAt
+            }
+    }
+    `
+    const result = await request(graphqlAPI, query);
+
+    return result.posts;
+}
+
+export const getSimilarPosts = async () => {
+    const query = gql`
+      query GetPostDetails($slug: String!, $categories: [String!]) {
+        posts(
+            where: { slug_not: $slug, AND: { categories_some: { slug_in: $categories}}}
+           last: 3 
+        ) {
+            title
+            featuredImage{
+                url
+            }
+            slug
+            createdAt
+        }
+      }
+    `
+
+    const result = await request(graphqlAPI, query);
+
+    return result.posts;
+}
+
+
+export const getCategories = async () => {
+    const query = gql`
+        query GetCategories {
+            categories {
+                name
+                slug
+            }
+        }
+    `;
+    const result = await request(graphqlAPI, query);
+
+    return result.categories;
+
 }
